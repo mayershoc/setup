931cda045982774ea8bfc7b22728f2e04d2c7d58
Christopher Mayershofer <christopher.mayershofer@gmx.de>
2021-05-12 22:50:18 +0200

    Add install scripts for basic tools after minimal ubuntu installation

---
+++ install_basics.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# This script install basic tools useful for a desktop usage after installing Ubuntu minimal
+
+# Git
+sudo apt install -y git
+# VLC media player
+sudo apt install -y vlc
+# Libreoffice suite
+sudo apt install -y libreoffice-gnome libreoffice libreoffice-l10n-de
+# Disk usage analyzer
+sudo apt install -y baobab
+# Image manipulation
+sudo apt install -y shotwell
+# Remote desktop client
+sudo apt install -y remmina
+
+
